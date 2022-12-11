5024498a2ea590487ee82c515cc3de79f78aa5fc
Devika18 <devikankarpe@gmail.com>
2022-12-11 21:25:14 +0530

    added: logic for invoice service providing invoice based on user id

--- src/main/java/org/example/InvoiceSummary.java
+++ src/main/java/org/example/InvoiceSummary.java
@@ -3,12 +3,11 @@ package org.example;
 public class InvoiceSummary {
     private final int numOfRides;
     private final double totalFare;
-    private final double averageFare;
+    private double averageFare;
 
     public InvoiceSummary(int numOfRides, double totalFare) {
         this.numOfRides = numOfRides;
         this.totalFare = totalFare;
-        this.averageFare = this.totalFare/ this.numOfRides;
     }
 
     @Override
@@ -17,5 +16,10 @@ public class InvoiceSummary {
         if (o == null || getClass() != o.getClass()) return false;
         InvoiceSummary that = (InvoiceSummary) o;
         return numOfRides == that.numOfRides && Double.compare(that.totalFare, totalFare) == 0 && Double.compare(that.averageFare, averageFare) == 0;
+    }
+
+    public InvoiceSummary getInvoiceSummary() {
+        this.averageFare = this.totalFare / this.numOfRides;
+        return this;
     }
 }
